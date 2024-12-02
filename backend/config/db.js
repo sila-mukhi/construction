75eb130de88bf12a7f2882662ed9e54919ae47ec
Sila Mukhi <silamukhi165@gmail.com>
2024-12-02 15:05:44 +0530

    some changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 export const connectDB = async () =>{
-    await mongoose.connect('mongodb://localhost:27017/construction').then(()=>console.log("db connected"));
-    // await mongoose.connect('mongodb+srv://silamukhi:sila123@cluster0.dcu8v.mongodb.net/course').then(()=>console.log("db connected"));
+    // await mongoose.connpect('mongodb://localhost:27017/construction').then(()=>console.log("db connected"));
+    await mongoose.connect('mongodb+srv://silamukhi:sila123@cluster0.zz9kz.mongodb.net/construction').then(()=>console.log("db connected"));
+    // mongodb+srv://silamukhi:<db_password>@cluster0.zz9kz.mongodb.net/construction
+    // mongodb+srv://silamukhi:sila1234@cluster0.zz9kz.mongodb.net/?
+
 }
+
